47ffec858983ed5d2692caa81ae7dad66ab92092
kvernet <kverner47@gmail.com>
2023-06-24 14:33:59 -0300

    commit dia 23/06

--- src/componets/CardLivro.vue
+++ src/componets/CardLivro.vue
@@ -1,10 +1,15 @@
 <script setup>
-import { adicionarAoCarrinho } from '@/_data/carrinho.js';
+import MButton from './MButton.vue'
+
 const props = defineProps({
-    livro: Object 
+  livro: Object
 })
 
-import MButton from './MButton.vue';
+const emit = defineEmits(['adicionarAoCarrinho']);
+
+import CartPlus from 'vue-material-design-icons/CartPlus.vue';
+import ShareVariant from 'vue-material-design-icons/ShareVariant.vue';
+import HeartPlus from 'vue-material-design-icons/HeartPlus.vue';
 
 function formatarPreco(preco) {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
@@ -12,20 +17,24 @@ function formatarPreco(preco) {
 </script>
 
 <template>
-    <div class="card-livro">
-        <div class="card-info-livro">
-          <div class="wrap-livro">
-            <img :src="props.livro.img" alt="Capa do livro" class="capa-livro" />
-          </div>
-          <p class="titulo-livro">{{ props.livro.title }}</p>
-          <p class="autor-livro">{{ props.livro.author }}</p>
-          <p class="preco-livro">{{ formatarPreco(props.livro.price) }}</p>
-        </div>
-        <div class="card-buttons-livros">
-          <m-button @click="adicionarAoCarrinho(props.livro)" texto = "Adicionar ao carrinho"/>
-          <m-button texto = "Favoritar" />
-        </div>
+  <div class="card-livro">
+    <div class="card-info-livro">
+      <div class="wrap-livro">
+        <img :src="props.livro.img" alt="Capa do livro" class="capa-livro" />
       </div>
+      <p class="titulo-livro">{{ props.livro.title }}</p>
+      <p class="autor-livro">{{ props.livro.author }}</p>
+      <p class="preco-livro">{{ formatarPreco(props.livro.price) }}</p>
+    </div>
+    <div class="card-buttons-livros">
+      <m-button class="secundario" @click="emit('adicionarAoCarrinho', props.livro)">
+        <cart-plus />
+      </m-button>
+      <m-button class="sucesso"> <share-variant /> </m-button>
+      <m-button class="perigo"> <heart-plus /> </m-button>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
